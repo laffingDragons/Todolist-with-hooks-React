b967102f8f65aaaa9e7a11dbd39fbf4de3b91627
Akshay <akshay.patil@wisdomleaf.com>
2025-04-15 12:40:58 +0530

    `Refactored TodoList component with improved state management and added features`

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef, useEffect, useCallback } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import TodoLists from "./TodoLists";
+import TodoLists from './TodoLists';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { TouchBackend } from 'react-dnd-touch-backend';
 
 function App() {
-
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
 
   const isMobile = () => {
     return /Mobi|Android/i.test(navigator.userAgent);
@@ -17,38 +16,39 @@ function App() {
   useEffect(() => {
     const storedTodos = JSON.parse(localStorage.getItem('todos'));
     if (storedTodos) setTodos(storedTodos);
-  }, [])
+  }, []);
 
   useEffect(() => {
-    if (todos.length) localStorage.setItem('todos', JSON.stringify(todos))
-  }, [todos])
+    const handler = setTimeout(() => {
+      if (todos.length) localStorage.setItem('todos', JSON.stringify(todos));
+    }, 500);
+    return () => clearTimeout(handler);
+  }, [todos]);
 
-  const todoNameRef = useRef()
+  const todoNameRef = useRef();
 
-  function addTodo(e) {
-    const name = todoNameRef.current.value
-    console.log(todoNameRef.current.value);
+  const addTodo = useCallback(() => {
+    const name = todoNameRef.current.value;
     if (name) {
-      setTodos(preTodos => {
-        return [
-          {
-            id: uuidv4(),
-            name: name,
-            complete: false,
-            createdOn: new Date(),
-            completedOn: '',
-            subTasks: [],
-          },
-          ...preTodos]
-      })
-      todoNameRef.current.value = ''
+      setTodos((prevTodos) => [
+        {
+          id: uuidv4(),
+          name: name,
+          complete: false,
+          createdOn: new Date(),
+          completedOn: '',
+          subTasks: [],
+        },
+        ...prevTodos,
+      ]);
+      todoNameRef.current.value = '';
     }
-  }
-  const toggleTodo = (id, isSubTask = false, parentId = null) => {
+  }, []);
+
+  const toggleTodo = useCallback((id, isSubTask = false, parentId = null) => {
     setTodos((prevTodos) => {
       const newTodos = prevTodos.map((todo) => {
         if (isSubTask && todo.id === parentId) {
-          // Find the subTask inside the parent task
           const updatedSubTasks = todo.subTasks.map((subTask) => {
             if (subTask.id === id) {
               return {
@@ -59,76 +59,173 @@ function App() {
             }
             return subTask;
           });
-
-          return {
-            ...todo,
-            subTasks: updatedSubTasks,
-          };
+          return { ...todo, subTasks: updatedSubTasks };
         } else if (!isSubTask && todo.id === id) {
-          // Toggle the main task
           return {
             ...todo,
             complete: !todo.complete,
             completedOn: todo.complete ? '' : new Date(),
           };
         }
-        return todo; // Return the task unchanged if no match
+        return todo;
       });
-
       return newTodos;
     });
-  };
-  // Reorder todos after drag
-  function moveTodo(dragIndex, hoverIndex) {
-    const dragTodo = todos[dragIndex];
-    const newTodos = [...todos];
-    newTodos.splice(dragIndex, 1);
-    newTodos.splice(hoverIndex, 0, dragTodo);
-    setTodos(newTodos);
-  }
+  }, []);
 
-  function completeTodos() {
-    console.log("🚀 ~ file:leteTodos ~ completeTodos")
-    const newTodos = todos.filter(x => !x.complete);
-    setTodos(newTodos);
-  }
+  const updateTodo = useCallback((id, newName, isSubTask = false, parentId = null) => {
+    setTodos((prevTodos) => {
+      const newTodos = prevTodos.map((todo) => {
+        if (isSubTask && todo.id === parentId) {
+          return {
+            ...todo,
+            subTasks: todo.subTasks.map((subTask) =>
+              subTask.id === id ? { ...subTask, name: newName } : subTask
+            ),
+          };
+        } else if (!isSubTask && todo.id === id) {
+          return { ...todo, name: newName };
+        }
+        return todo;
+      });
+      return newTodos;
+    });
+  }, []);
+
+  const deleteTodo = useCallback((id, isSubTask = false, parentId = null) => {
+    setTodos((prevTodos) => {
+      if (isSubTask && parentId) {
+        return prevTodos.map((todo) =>
+          todo.id === parentId
+            ? { ...todo, subTasks: todo.subTasks.filter((subTask) => subTask.id !== id) }
+            : todo
+        );
+      }
+      return prevTodos.filter((todo) => todo.id !== id);
+    });
+  }, []);
+
+  const moveTodo = useCallback((dragIndex, hoverIndex, parentId = null) => {
+    setTodos((prevTodos) => {
+      if (parentId) {
+        const newTodos = [...prevTodos];
+        const parentTodo = newTodos.find((todo) => todo.id === parentId);
+        const dragSubTask = parentTodo.subTasks[dragIndex];
+        parentTodo.subTasks.splice(dragIndex, 1);
+        parentTodo.subTasks.splice(hoverIndex, 0, dragSubTask);
+        return newTodos;
+      } else {
+        const dragTodo = prevTodos[dragIndex];
+        const newTodos = [...prevTodos];
+        newTodos.splice(dragIndex, 1);
+        newTodos.splice(hoverIndex, 0, dragTodo);
+        return newTodos;
+      }
+    });
+  }, []);
+
+  const completeTodos = useCallback(() => {
+    setTodos((prevTodos) => prevTodos.filter((x) => !x.complete));
+  }, []);
 
   return (
     <DndProvider backend={isMobile() ? TouchBackend : HTML5Backend}>
       <>
-        <div className='z-100 conatiner'>
-          <h1 className='primary-text f-huge text-center'>Todolist</h1>
-          <div className=' text-center d-block d-sm-none sticky' >
-            <input ref={todoNameRef} type="text" className='add-input' onKeyPress={event => {
-              if (event.key === 'Enter') {
-                addTodo();
-              }
-            }} />
-            <button onClick={addTodo} className='primary-btn'>Add todo</button>
+        <div className="z-100 conatiner">
+          <h1 className="primary-text f-huge text-center">Todolist</h1>
+          <div className="text-center d-block d-sm-none sticky">
+            <input
+              ref={todoNameRef}
+              type="text"
+              className="add-input"
+              onKeyPress={(event) => {
+                if (event.key === 'Enter') {
+                  addTodo();
+                }
+              }}
+            />
+            <button onClick={addTodo} className="primary-btn">
+              Add todo
+            </button>
             <br />
           </div>
 
-          <div className='primary-text'>{todos.filter(x => !x.complete).length} todos left todo</div>
-          <TodoLists todos={todos.slice().reverse()} toggleTodo={toggleTodo} moveTodo={moveTodo} setTodos={setTodos} />
-          <button onClick={completeTodos} className="mt-4 secondary-btn">Remove all checked todo</button>
-
-
+          <div className="primary-text">{todos.filter((x) => !x.complete).length} todos left</div>
+          <TodoLists
+            todos={todos.slice().reverse()}
+            toggleTodo={toggleTodo}
+            moveTodo={moveTodo}
+            setTodos={setTodos}
+            updateTodo={updateTodo}
+            deleteTodo={deleteTodo}
+          />
+          <button onClick={completeTodos} className="mt-4 secondary-btn">
+            Remove all checked todo
+          </button>
         </div>
-
-
 
         <div className="bg-container">
           <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="500px">
-            <path id="shape1" fillRule="evenodd" fill="rgb(35, 23, 123)" d="M-0.011,9.318 L8.682,6.988 L11.011,15.682 L2.318,18.011 L-0.011,9.318 Z"></path>
-            <path id="shape8" fillRule="evenodd" fill="rgb(35, 23, 123)" d="M607.694,164.319 L612.680,161.693 L615.306,166.680 L610.320,169.305 L607.694,164.319 Z"></path>
-            <path id="shape9" fillRule="evenodd" fill="rgb(93, 203, 250)" d="M667.343,205.646 L679.152,213.179 L671.620,224.988 L659.810,217.456 L667.343,205.646 Z"></path>
-            <path id="shape10" fillRule="evenodd" fill="rgb(93, 203, 250)" d="M915.839,-0.008 L924.997,5.833 L919.156,14.991 L909.998,9.150 L915.839,-0.008 Z"></path>
-            <path id="cir6" fillRule="evenodd" fill="rgb(35, 23, 123)" d="M989.000,93.000 C993.418,93.000 997.000,96.582 997.000,101.000 C997.000,105.418 993.418,109.000 989.000,109.000 C984.582,109.000 981.000,105.418 981.000,101.000 C981.000,96.582 984.582,93.000 989.000,93.000 Z"></path>
-            <path id="cir5" fillRule="evenodd" fill="rgb(35, 23, 123)" d="M1165.000,8.000 C1167.761,8.000 1170.000,10.238 1170.000,13.000 C1170.000,15.761 1167.761,18.000 1165.000,18.000 C1162.239,18.000 1160.000,15.761 1160.000,13.000 C1160.000,10.238 1162.239,8.000 1165.000,8.000 Z"></path>
-            <path id="cir4" fillRule="evenodd" fill="rgb(93, 203, 250)" d="M1303.500,118.000 C1304.881,118.000 1306.000,119.119 1306.000,120.500 C1306.000,121.880 1304.881,123.000 1303.500,123.000 C1302.119,123.000 1301.000,121.880 1301.000,120.500 C1301.000,119.119 1302.119,118.000 1303.500,118.000 Z"></path>
-            <path id="cir3" fillRule="evenodd" fill="rgb(93, 203, 250)" d="M61.000,112.000 C62.657,112.000 64.000,113.343 64.000,115.000 C64.000,116.657 62.657,118.000 61.000,118.000 C59.343,118.000 58.000,116.657 58.000,115.000 C58.000,113.343 59.343,112.000 61.000,112.000 Z"></path>
-            <path id="cir2" fillRule="evenodd" fill="rgb(74, 92, 246)" d="M265.500,-0.000 C271.299,-0.000 276.000,4.701 276.000,10.500 C276.000,16.299 271.299,21.000 265.500,21.000 C259.701,21.000 255.000,16.299 255.000,10.500 C255.000,4.701 259.701,-0.000 265.500,-0.000 Z"></path>
-            <path id="cir1" fillRule="evenodd" fill="rgb(35, 23, 123)" d="M185.500,131.000 C187.985,131.000 190.000,133.015 190.000,135.500 C190.000,137.985 187.985,140.000 185.500,140.000 C183.015,140.000 181.000,137.985 181.000,135.500 C181.000,133.015 183.015,131.000 185.500,131.000 Z"></path>
+            <path
+              id="shape1"
+              fillRule="evenodd"
+              fill="rgb(35, 23, 123)"
+              d="M-0.011,9.318 L8.682,6.988 L11.011,15.682 L2.318,18.011 L-0.011,9.318 Z"
+            ></path>
+            <path
+              id="shape8"
+              fillRule="evenodd"
+              fill="rgb(35, 23, 123)"
+              d="M607.694,164.319 L612.680,161.693 L615.306,166.680 L610.320,169.305 L607.694,164.319 Z"
+            ></path>
+            <path
+              id="shape9"
+              fillRule="evenodd"
+              fill="rgb(93, 203, 250)"
+              d="M667.343,205.646 L679.152,213.179 L671.620,224.988 L659.810,217.456 L667.343,205.646 Z"
+            ></path>
+            <path
+              id="shape10"
+              fillRule="evenodd"
+              fill="rgb(93, 203, 250)"
+              d="M915.839,-0.008 L924.997,5.833 L919.156,14.991 L909.998,9.150 L915.839,-0.008 Z"
+            ></path>
+            <path
+              id="cir6"
+              fillRule="evenodd"
+              fill="rgb(35, 23, 123)"
+              d="M989.000,93.000 C993.418,93.000 997.000,96.582 997.000,101.000 C997.000,105.418 993.418,109.000 989.000,109.000 C984.582,109.000 981.000,105.418 981.000,101.000 C981.000,96.582 984.582,93.000 989.000,93.000 Z"
+            ></path>
+            <path
+              id="cir5"
+              fillRule="evenodd"
+              fill="rgb(35, 23, 123)"
+              d="M1165.000,8.000 C1167.761,8.000 1170.000,10.238 1170.000,13.000 C1170.000,15.761 1167.761,18.000 1165.000,18.000 C1162.239,18.000 1160.000,15.761 1160.000,13.000 C1160.000,10.238 1162.239,8.000 1165.000,8.000 Z"
+            ></path>
+            <path
+              id="cir4"
+              fillRule="evenodd"
+              fill="rgb(93, 203, 250)"
+              d="M1303.500,118.000 C1304.881,118.000 1306.000,119.119 1306.000,120.500 C1306.000,121.880 1304.881,123.000 1303.500,123.000 C1302.119,123.000 1301.000,121.880 1301.000,120.500 C1301.000,119.119 1302.119,118.000 1303.500,118.000 Z"
+            ></path>
+            <path
+              id="cir3"
+              fillRule="evenodd"
+              fill="rgb(93, 203, 250)"
+              d="M61.000,112.000 C62.657,112.000 64.000,113.343 64.000,115.000 C64.000,116.657 62.657,118.000 61.000,118.000 C59.343,118.000 58.000,116.657 58.000,115.000 C58.000,113.343 59.343,112.000 61.000,112.000 Z"
+            ></path>
+            <path
+              id="cir2"
+              fillRule="evenodd"
+              fill="rgb(74, 92, 246)"
+              d="M265.500,-0.000 C271.299,-0.000 276.000,4.701 276.000,10.500 C276.000,16.299 271.299,21.000 265.500,21.000 C259.701,21.000 255.000,16.299 255.000,10.500 C255.000,4.701 259.701,-0.000 265.500,-0.000 Z"
+            ></path>
+            <path
+              id="cir1"
+              fillRule="evenodd"
+              fill="rgb(35, 23, 123)"
+              d="M185.500,131.000 C187.985,131.000 190.000,133.015 190.000,135.500 C190.000,137.985 187.985,140.000 185.500,140.000 C183.015,140.000 181.000,137.985 181.000,135.500 C181.000,133.015 183.015,131.000 185.500,131.000 Z"
+            ></path>
           </svg>
         </div>
         <div className="box">
@@ -138,8 +235,7 @@ function App() {
         </div>
       </>
     </DndProvider>
-  )
-
+  );
 }
 
 export default App;
--- src/TodoLists.js
+++ src/TodoLists.js
@@ -1,118 +1,161 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState, useCallback } from 'react';
 import { useDrag, useDrop } from 'react-dnd';
 import * as moment from 'moment';
 import anime from 'animejs';
 import { v4 as uuidv4 } from 'uuid';
 
-const TodoItem = ({ todo, index, moveTodo, toggleTodo, parentId = null, isSubTask = false, addSubTask }) => {
-    const [{ isDragging }, drag] = useDrag({
-        type: 'TODO',
-        item: { index, parentId },
-        collect: (monitor) => ({
-            isDragging: monitor.isDragging(),
-        }),
-    });
+const TodoItem = React.memo(
+    ({ todo, index, moveTodo, toggleTodo, parentId = null, isSubTask = false, addSubTask, updateTodo, deleteTodo }) => {
+        const [{ isDragging }, drag] = useDrag({
+            type: 'TODO',
+            item: { index, parentId },
+            collect: (monitor) => ({
+                isDragging: monitor.isDragging(),
+            }),
+        });
 
-    const [, drop] = useDrop({
-        accept: 'TODO',
-        hover: (item) => {
-            if (item.index !== index) {
-                moveTodo(item.index, index, parentId);
-                item.index = index;
+        const [, drop] = useDrop({
+            accept: 'TODO',
+            hover: (item) => {
+                if (item.index !== index || item.parentId !== parentId) {
+                    moveTodo(item.index, index, item.parentId || parentId);
+                    item.index = index;
+                    item.parentId = parentId;
+                }
+            },
+        });
+
+        const taskRef = useRef();
+        const [isEditing, setIsEditing] = useState(false);
+        const [editValue, setEditValue] = useState(todo.name);
+
+        useEffect(() => {
+            if (!isDragging) {
+                anime({
+                    targets: taskRef.current,
+                    opacity: [0, 1],
+                    translateX: [10, 0],
+                    easing: 'easeOutQuad',
+                    duration: 300,
+                });
             }
-        },
-    });
+        }, [isDragging]);
 
-    const taskRef = useRef();
+        const handleCheck = (e) => {
+            toggleTodo(todo.id, isSubTask, parentId);
+            e.preventDefault();
+        };
 
-    useEffect(() => {
-        if (!isDragging) {
-            anime({
-                targets: taskRef.current,
-                opacity: [0, 1],
-                translateX: [10, 0],
-                easing: 'easeOutQuad',
-                duration: 300,
-            });
-        }
-    }, [isDragging]);
+        const handleEdit = () => {
+            if (isEditing && editValue.trim()) {
+                updateTodo(todo.id, editValue, isSubTask, parentId);
+            }
+            setIsEditing(!isEditing);
+        };
 
-    const handleCheck = (e) => {
-        toggleTodo(todo.id, isSubTask, parentId);
-        e.preventDefault();
-        console.log(" ??????", todo.id, isSubTask, parentId);
-    };
-
-
-    return (
-        <div
-            ref={(node) => { drag(drop(node)); taskRef.current = node; }}
-            className="p-r mt-4"
-            style={{ marginLeft: isSubTask ? '20px' : '0px' }} // Add indentation for subtasks
-            onClick={(e) => handleCheck(e)}
-        >
-            <input
-                type="checkbox"
-                className={isSubTask ? "_checkboxId-blue" : "_checkboxId"}
-                checked={todo.complete}
-            />
-            <label htmlFor="_checkbox">
-                <div id="tick_mark"></div>
-            </label>
-            <div className="ml-4 title-todo">{todo.name}</div>
-            {isSubTask && (
-                <div className="ml-4 date-todo">
-                    {todo.completedOn ? (
-                        <div className="sucess">{moment(todo.completedOn).fromNow()}</div>
-                    ) : (
-                        todo.createdOn && moment(todo.createdOn).fromNow()
-                    )}
-                </div>
-            )}
-            {/* Show "Add subtask" icon */}
-            {!isSubTask && (
+        return (
+            <div
+                ref={(node) => {
+                    drag(drop(node));
+                    taskRef.current = node;
+                }}
+                className="p-r mt-4"
+                style={{ marginLeft: isSubTask ? '20px' : '0px' }}
+                onClick={(e) => handleCheck(e)}
+            >
+                <input
+                    type="checkbox"
+                    id={`checkbox-${todo.id}`}
+                    aria-label={`Mark ${todo.name} as ${todo.complete ? 'incomplete' : 'complete'}`}
+                    className={isSubTask ? '_checkboxId-blue' : '_checkboxId'}
+                    checked={todo.complete}
+                />
+                <label htmlFor={`checkbox-${todo.id}`}>
+                    <div id="tick_mark"></div>
+                </label>
+                {isEditing ? (
+                    <input
+                        type="text"
+                        value={editValue}
+                        onChange={(e) => setEditValue(e.target.value)}
+                        onBlur={handleEdit}
+                        onKeyDown={(e) => {
+                            if (e.key === 'Enter') handleEdit();
+                        }}
+                        className="ml-4 title-todo add-input"
+                        autoFocus
+                    />
+                ) : (
+                    <div className="ml-4 title-todo" onDoubleClick={() => setIsEditing(true)}>
+                        {todo.name}
+                    </div>
+                )}
+                {isSubTask && (
+                    <div className="ml-4 date-todo">
+                        {todo.completedOn ? (
+                            <div className="sucess">{moment(todo.completedOn).fromNow()}</div>
+                        ) : (
+                            todo.createdOn && moment(todo.createdOn).fromNow()
+                        )}
+                    </div>
+                )}
                 <div
-                    className="add-subtask-icon"
+                    className="delete-icon"
                     onClick={(e) => {
                         e.stopPropagation();
-                        addSubTask(todo.id); // Show input to add subtask
+                        deleteTodo(todo.id, isSubTask, parentId);
                     }}
                 >
-                    ➕
+                    🗑️
                 </div>
-            )}
-        </div>
-    );
-};
+                {!isSubTask && (
+                    <div
+                        className="add-subtask-icon"
+                        onClick={(e) => {
+                            e.stopPropagation();
+                            addSubTask(todo.id);
+                        }}
+                    >
+                        ➕
+                    </div>
+                )}
+            </div>
+        );
+    }
+);
 
-const TodoLists = ({ todos, toggleTodo, moveTodo, setTodos }) => {
-    const [showSubtaskInput, setShowSubtaskInput] = useState(null); // To handle visibility of the subtask input
+const TodoLists = ({ todos, toggleTodo, moveTodo, setTodos, updateTodo, deleteTodo }) => {
+    const [showSubtaskInput, setShowSubtaskInput] = useState(null);
+    const [newSubtask, setNewSubtask] = useState('');
+    const inputRef = useRef();
 
-    const [newSubtask, setNewSubtask] = useState(''); // Store subtask input
-
-    const addSubTask = (parentId) => {
+    const addSubTask = useCallback((parentId) => {
         setShowSubtaskInput(parentId);
-    };
+        setTimeout(() => inputRef.current?.focus(), 0);
+    }, []);
 
-    const handleAddSubTask = (parentId) => {
-        if (newSubtask.trim() !== '') {
-            setTodos((prevTodos) => {
-                const updatedTodos = [...prevTodos];
-                const parentTask = updatedTodos.find((todo) => todo.id === parentId);
-                parentTask.subTasks.push({
-                    id: uuidv4(),
-                    name: newSubtask,
-                    complete: false,
-                    createdOn: new Date(),
-                    completedOn: '',
-                    type: 'subTask',
+    const handleAddSubTask = useCallback(
+        (parentId) => {
+            if (newSubtask.trim() !== '') {
+                setTodos((prevTodos) => {
+                    const updatedTodos = [...prevTodos];
+                    const parentTask = updatedTodos.find((todo) => todo.id === parentId);
+                    parentTask.subTasks.push({
+                        id: uuidv4(),
+                        name: newSubtask,
+                        complete: false,
+                        createdOn: new Date(),
+                        completedOn: '',
+                        type: 'subTask',
+                    });
+                    return updatedTodos;
                 });
-                return updatedTodos;
-            });
-            setNewSubtask('');
-            setShowSubtaskInput(null); // Hide the input after adding
-        }
-    };
+                setNewSubtask('');
+                setShowSubtaskInput(null);
+            }
+        },
+        [newSubtask, setTodos]
+    );
 
     return (
         <div className="todo-list">
@@ -124,12 +167,20 @@ const TodoLists = ({ todos, toggleTodo, moveTodo, setTodos }) => {
                         moveTodo={moveTodo}
                         toggleTodo={toggleTodo}
                         addSubTask={addSubTask}
+                        updateTodo={updateTodo}
+                        deleteTodo={deleteTodo}
                     />
-                    <div className="ml-4 date-todo">{todo.completedOn ? <div className="sucess">{moment(todo.completedOn).fromNow()}</div> : todo.createdOn && moment(todo.createdOn).fromNow()}</div>
-                    {/* Display input for subtask if it's the parent task and showSubtaskInput matches the parent task ID */}
+                    <div className="ml-4 date-todo">
+                        {todo.completedOn ? (
+                            <div className="sucess">{moment(todo.completedOn).fromNow()}</div>
+                        ) : (
+                            todo.createdOn && moment(todo.createdOn).fromNow()
+                        )}
+                    </div>
                     {showSubtaskInput === todo.id && (
                         <div className="subtask-input-container">
                             <input
+                                ref={inputRef}
                                 type="text"
                                 value={newSubtask}
                                 onChange={(e) => setNewSubtask(e.target.value)}
@@ -138,13 +189,27 @@ const TodoLists = ({ todos, toggleTodo, moveTodo, setTodos }) => {
                                     if (e.key === 'Enter') {
                                         handleAddSubTask(todo.id);
                                     }
+                                    if (e.key === 'Escape') {
+                                        setShowSubtaskInput(null);
+                                        setNewSubtask('');
+                                    }
                                 }}
-                                className='add-input'
+                                className="add-input"
                             />
-                            <button className='primary-btn' onClick={() => handleAddSubTask(todo.id)}>Add Subtask</button>
+                            <button className="primary-btn" onClick={() => handleAddSubTask(todo.id)}>
+                                Add Subtask
+                            </button>
+                            <button
+                                className="secondary-btn"
+                                onClick={() => {
+                                    setShowSubtaskInput(null);
+                                    setNewSubtask('');
+                                }}
+                            >
+                                Cancel
+                            </button>
                         </div>
                     )}
-                    {/* Render subtasks if any */}
                     {todo.subTasks && todo.subTasks.length > 0 && (
                         <div>
                             {todo.subTasks.map((subTask, subIndex) => (
@@ -154,8 +219,10 @@ const TodoLists = ({ todos, toggleTodo, moveTodo, setTodos }) => {
                                     index={subIndex}
                                     moveTodo={moveTodo}
                                     toggleTodo={toggleTodo}
-                                    parentId={todo.id} // Pass parentId for subtasks
-                                    isSubTask={true} // Mark this as a subtask
+                                    parentId={todo.id}
+                                    isSubTask={true}
+                                    updateTodo={updateTodo}
+                                    deleteTodo={deleteTodo}
                                 />
                             ))}
                         </div>
